f7565f9d1b62e0e0910f8c221ec3303ee02b276c
jwhsieh <JaneHsieh@janes-macbook-air.local>
2017-12-28 04:01:14 -0500

    Testing Git and C++ compiler

--- C++ Exercises/main.cpp
+++ C++ Exercises/main.cpp
@@ -1,15 +1,15 @@
-//
+//  Kattis C++ Exercises
 //  main.cpp
 //  C++ Exercises
 //
 //  Created by Jane Hsieh on 12/28/17.
 //  Copyright © 2017 Jane Hsieh. All rights reserved.
-//
+//  12/28 configuring Git source control
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!\n" << "What good fun!";
     return 0;
 }
