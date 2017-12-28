21c068c71ad40227165ccd45de12104eec12212a
jwhsieh <JaneHsieh@janes-macbook-air.local>
2017-12-28 03:34:16 -0500

    Initial Commit

---
+++ C++ Exercises/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ Exercises
+//
+//  Created by Jane Hsieh on 12/28/17.
+//  Copyright © 2017 Jane Hsieh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
